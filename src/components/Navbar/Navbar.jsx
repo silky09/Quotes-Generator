4b378f4a65f9dcb9c57e1f0f4009245cbd26cdb6
silky <silkysantosh09@gmail.com>
2021-05-21 19:25:45 +0200

    Added icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,18 +12,18 @@ const Navbar = () => {
           <Link to="/"  data-target="mobile-demo" className="sidenav-trigger right"><i className="material-icons">menu</i></Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><NavLink to="/"><i className="material-icons">home</i></NavLink></li>
-        <li><NavLink to="/quotes"><i className="material-icons">format_quote</i></NavLink></li>
         <li><NavLink to="/music"><i className="material-icons">queue_music</i></NavLink></li>
-            <li><NavLink to="/games"><i className="material-icons">videogame_asset</i></NavLink></li>
+        <li><NavLink to="/games"><i className="material-icons">videogame_asset</i></NavLink></li>
+        <li><NavLink to="/quotes"><i className="material-icons">format_quote</i></NavLink></li>
             {/* <li><NavLink to="/covid19"><i className="material-icons">coronavirus</i></NavLink></li> */}
     </ul>
   </div>
     </nav>
     <ul className="sidenav" id="mobile-demo">
        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/quotes">Quotes</NavLink></li>
         <li><NavLink to="/music">Music</NavLink></li>
         <li><NavLink to="/games">Games</NavLink></li>
+        <li><NavLink to="/quotes">Quotes</NavLink></li>
       </ul>
       </>
   )
