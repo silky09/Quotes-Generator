8d96d76ccd8c72fa90d78ca83d0a94cd7d575ac7
silky <silkysantosh09@gmail.com>
2021-05-21 12:29:13 +0200

    Added new folder after deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 // eslint-disable-next-line
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Quotes from './components/Quotes/Quotes'
-import Music from './components/Music'
+import Music from './components/Music/Music'
 import Games from './components/Games/Games'
 // import Covid19 from './components/Covid19/Covid19';
 import "./App.css"
